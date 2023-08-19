e59f975c831e3fd7f5e32259d6f72e24499c210c
OwenGoCrazy <125096932+OwenGoCrazy@users.noreply.github.com>
2023-08-19 19:37:08 -0400

    Update LoadString.lua

--- Code/LoadString.lua
+++ Code/LoadString.lua
@@ -1,3 +1,7 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/Lock-Load.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/Anti-Load.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/Spoof.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/No-Bullet-Delay.lua"))()
+
+
 
