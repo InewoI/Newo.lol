cb79b973041d0aef1cdd0cf617c2c0bac8f6cf31
InewoI <149034196+InewoI@users.noreply.github.com>
2023-11-18 09:42:32 -0500

    Update LoadString.lua

--- Code/LoadString.lua
+++ Code/LoadString.lua
@@ -1,6 +1,6 @@
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/Lock-Load.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/Spoof.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/OwenGoCrazy/Newo.lol/main/Code/No-Bullet-Delay.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/Lock-Load.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/Spoof.lua"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/No-Bullet-Delay.lua"))()
 
 
 
