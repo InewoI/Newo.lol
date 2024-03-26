0fc926c5e544fb976d78b8ae7e06266df114f784
InewoI <149034196+InewoI@users.noreply.github.com>
2024-03-26 00:50:41 -0400

    Update LoadString.lua

--- Code/LoadString.lua
+++ Code/LoadString.lua
@@ -1,5 +1,4 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/Lock-Load.lua"))()
-loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/Spoof.lua"))()
 loadstring(game:HttpGet("https://raw.githubusercontent.com/InewoI/Newo.lol/main/Code/No-Bullet-Delay.lua"))()
 
 
